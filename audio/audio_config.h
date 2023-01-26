2ad02fd2cc794da341bb0eb70e4942432af79779
Laurent Lardinois <19912792+type-one@users.noreply.github.com>
2023-01-26 23:07:03 +0100

    fix gcc 10 linux linking, add linux tuning parameters

--- audio/audio_config.h
+++ audio/audio_config.h
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------//
-// Audio Context - Spare time development for fun                              //
+// Audio Config - Spare time development for fun                               //
 // (c) 2023 Laurent Lardinois https://be.linkedin.com/in/laurentlardinois      //
 //                                                                             //
 // https://github.com/type-one/CSideTone                                       //
@@ -23,40 +23,43 @@
 
 #pragma once
 
-#if !defined(__AUDIO_CONTEXT_H__)
-#define __AUDIO_CONTEXT_H__
-
-#include "audio_config.h"
+#if !defined(__AUDIO_CONFIG_H__)
+#define __AUDIO_CONFIG_H__
 
 #include <stdbool.h>
 #include <stdint.h>
 
-#if defined(__cplusplus)
-extern "C"
-{
-#endif
+// uncomment to display producer/consumer progress
+// #define DISPLAY_PROGRESS
 
-    struct ma_context;
+// some values tested on Win10/Corei7/AVX2
+// 16000, 20ms, 1
+// 44100, 16ms, 1
+// 44100, 20ms, 2
+// 48000, 50ms, 1
+// 22050, 20ms, 1 (in debug)
+// 22050, 12ms, 1 (in release)
+// 22050, 8ms, 1  (in release, SIMD/Most optimizations)
+// 22050, 14ms, 2
+// under VMware player 17 (Ubuntu 20 LTS) on Win10/Corei7/AVX2 host
+// 22050, 40ms, 1 (in debug)
+// under Linux host
+// 22050, 8ms, 1 (in debug)
 
-#define is_of_type(x, type) _Generic(x, type : true, default : false)
-
-    struct audio_context
-    {
-        struct ma_context* m_context;
-    };
-
-#if defined(AUDIO_CONTEXT_IMPLEM)
-#define EXTERN_AUDIO_CONTEXT
+// parameters to tune
+#if defined(_WIN32)
+// increase if you hear clicks
+#define audio_period_ms 10    // works on Windows host, Core i7
 #else
-#define EXTERN_AUDIO_CONTEXT extern
+#define audio_period_ms 8     // works on Linux host, Core i7
+//#define audio_period_ms = 40; // works on Linux VM (vmplayer 17) under Windows host, Core i7
 #endif
 
-    EXTERN_AUDIO_CONTEXT int init_audio_context(struct audio_context* context);
-    EXTERN_AUDIO_CONTEXT int deinit_audio_context(struct audio_context* context);
+#define audio_frame_sync true // sync producer/consumer
+#define audio_frequency 22050
+#define audio_frame_size ((audio_frequency * audio_period_ms) / 1000)
+#define audio_channels 1  // mono (1), stereo (2)
+typedef int16_t sample_t; // S16_LE
+// typedef float sample_t;     // F32
 
-
-#if defined(__cplusplus)
-};
-#endif
-
-#endif //  __AUDIO_CONTEXT_H__
+#endif //  __AUDIO_CONFIG_H__
